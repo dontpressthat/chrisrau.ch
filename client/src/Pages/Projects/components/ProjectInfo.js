4b2f82d1a6fd036807ba1613961cacd9f10b91db
dontpressthat <chris@chrisrau.ch>
2020-06-07 15:31:43 -0700

    Update Projects page responsive behavior

---
+++ client/src/Pages/Projects/components/ProjectInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import StyledText from '../../../global-components/styled-components/StyledText';
+
+const Container = styled.div`
+
+`
+
+
+const ProjectInfo = () => {
+
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ProjectInfo;
--- client/src/Pages/Projects/components/Tile.js
+++ client/src/Pages/Projects/components/Tile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import StyledText from '../../../global-components/styled-components/StyledText';
 
@@ -7,17 +7,31 @@ const tileSize = 250;
 const Container = styled.div`
   display: flex;
   flex: 2 2 ${tileSize}px;
-  justify-content: center;
-  align-items: center;
   width: ${tileSize}px;
   height: ${tileSize}px;
   background-image: url(${props => props.image});
   background-color: ${props => props.color};
   background-size: cover;
   margin: 5px;
-  &:hover {
-    opacity: 0.3;
-  }
+`
+
+const Overlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  @media only screen and (max-width: 1280px) {
+    background-color: rgba(255, 255, 255, 0.25);
+  };
+  @media only screen and (min-width: 1280px) {
+    opacity: 0;
+    transition: 0.25s ease-out;
+    &:hover {
+      background-color: #4a544e;
+      opacity: 1;
+    };
+  };
 `
 
 const TextWrapper = styled.div`
@@ -25,25 +39,37 @@ const TextWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  @media only screen and (max-width: 1280px) {
+    color: #333333;
+  };
+  @media only screen and (min-width: 1280px) {
+    color: lightgray;
+  };
 `
 
 const Tile = ({ image, name, category }) => {
-  let color = 'gray';
-  if (category === 'Development') {
-    color = 'lightblue'
-  } else if (category === 'Design') {
-    color = 'lightgreen'
-  } else if (category === 'Photography') {
-    color = 'pink'
-  } else if (category === 'Rendering') {
-    color = 'darkseagreen'
-  } 
+
+  const [showBox, setShowBox] = useState(false);
+
+  let color = 'lightgray';
+  // if (category === 'Development') {
+  //   color = 'lightblue'
+  // } else if (category === 'Design') {
+  //   color = 'lightgreen'
+  // } else if (category === 'Photography') {
+  //   color = 'pink'
+  // } else if (category === 'Rendering') {
+  //   color = 'coral'
+  // }
+
   return (
-    <Container image={image} color={color}>
-      <TextWrapper>
-        <StyledText>{name}</StyledText>
-        <StyledText>{category}</StyledText>
-      </TextWrapper>
+    <Container image={image} color={color} onMouseEnter={() => setShowBox(true)} onMouseLeave={() => setShowBox(false)}>
+      <Overlay>
+        <TextWrapper>
+          <StyledText size={20} font='Ubuntu'>{name}</StyledText>
+          <StyledText size={12} weight={300} fontStyle='italic'>{category}</StyledText>
+        </TextWrapper>
+      </Overlay>
     </Container>
   )
 }
