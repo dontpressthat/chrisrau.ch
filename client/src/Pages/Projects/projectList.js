f0caf278c35ab8815a95dad5f47f529c5753c0cf
dontpressthat <chris@chrisrau.ch>
2020-06-22 23:19:53 -0700

    Make some changes to the chrisrau.ch description

--- client/src/Pages/Projects/projectList.js
+++ client/src/Pages/Projects/projectList.js
@@ -13,24 +13,24 @@ const projectList = [
     image: `/assets/images/1.jpg`,
     description:
       <Container>
-        <StyledText size={16}>
+        <StyledText size={16} color='#444444'>
           <p>
             chrisrau.ch is React single page application (SPA) built to serve as my personal portfolio website. The goal was to build a highly maintainable application tailored to the use case of showcasing my various projects and hobbies. In an effort to keep things lightweight, as well as provide a challenge, almost all app features were manually written as opposed to using readily available packages or libraries.
           </p>
           <p>
-            Because numerous elements on the project page are designed to display redundant information from each project, the project names, categories, images, descriptions, and GitHub links are all referenced from one single source of truth (SSoT). This prevents the need to edit multiple files with the same information, preventing potential discrepancies and/or errors. The categories filter bar is also programmatically built and renders all unique categories found in the project data file.
+            Because numerous elements on the project page are designed to display redundant information from each project, the project names, categories, images, descriptions, and GitHub links are all referenced from one single source of truth (SSoT). This prevents the need to edit multiple files with the same information, preventing potential discrepancies and/or errors. The categories filter bar is also programmatically built and rendered from all unique categories found in the project data file.
           </p>
           <p>
-            A large amount of attention was given to responsiveness and browser compatibility. All aspects of the website will actively respond to resizing down to a window width of just 300 pixels, and care was taken to only use widely-supported CSS properties for all core functionality. This ensures usability on all devices, whether it be mobile or desktop.
+            A large amount of attention was given to responsiveness and browser compatibility. All aspects of the website will actively respond to resizing down to a window width of just 300 pixels, and care was taken to only use widely-supported CSS properties for all core functionality. The behavior of various elements will also change completely depending on screen size. This ensures usability and positive user experience on all devices, whether it be mobile or desktop.
           </p>
           <p>Google's Firebase powers the backend of this application, acting as the webhost and server.</p>
         </StyledText>
-        <StyledText size={20}>
+        <StyledText size={20} color='#4a544e'>
           <p>
             Technologies Used:
           </p>
         </StyledText>
-        <StyledText size={16}>
+        <StyledText size={16} color='#444444'>
           <ul>
             <li>JavaScript ES6</li>
             <li>Babel</li>
@@ -47,12 +47,12 @@ const projectList = [
             <li>Git / GitHub</li>
           </ul>
         </StyledText>
-        <StyledText size={20}>
+        <StyledText size={20} color='#4a544e'>
           <p>
             Skills and Practices:
           </p>
         </StyledText>
-        <StyledText size={16}>
+        <StyledText size={16} color='#444444'>
           <ul>
             <li>Fully responsive across devices of all shapes and sizes</li>
             <li>DRY (Don't Repeat Yourself) principle
